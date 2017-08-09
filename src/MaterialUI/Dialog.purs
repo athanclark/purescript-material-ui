e6a8c15834af687d482eff68810443f8fb4d204d
Athan Clark <athan.clark@gmail.com>
2017-08-08 23:23:10 -0600

    proper dialog

--- src/MaterialUI/Dialog.purs
+++ src/MaterialUI/Dialog.purs
@@ -1,6 +1,9 @@
 module MaterialUI.Dialog
-  ( dialog', DialogProps, DialogPropsO
+  ( dialog', DialogProps, DialogPropsO, DialogClasses
+  , MaxWidth, xs, sm, md
   ) where
+
+import MaterialUI.Types (Styles)
 
 import Prelude
 import React (Event, ReactClass, createElement, ReactElement, ReactProps, ReactState, ReactRefs, ReadOnly, ReadWrite)
@@ -13,35 +16,56 @@ foreign import dialogImpl :: forall props. ReactClass props
 
 type DialogProps o =
   { open :: Boolean
-  , title :: Array ReactElement
   | o }
 
 
-type DialogPropsO eff =
-  ( actions                   :: Array ReactElement
-  , actionsContainerClassName :: String
-  , actionsContainerStyle     :: forall row. { | row }
-  , autoDetectWindowHeight    :: Boolean
-  , autoScrollBodyContent     :: Boolean
-  , bodyClassName             :: String
-  , bodyStyles                :: forall row. { | row }
-  , children                  :: Array ReactElement
-  , contentClassName          :: String
-  , contentStyle              :: forall row. { | row }
-  , modal                     :: Boolean
-  , onRequestClose            :: EffFn1 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Unit
-  , overlayClassName          :: String
-  , overlayStyle              :: forall row. { | row }
-  , paperClassName            :: String
-  , paperProps                :: forall row. { | row }
-  , repositionOnUpdate        :: Boolean
-  , style                     :: forall row. { | row }
-  , titleClassName            :: String
-  , titleStyle                :: forall row. { | row }
+newtype MaxWidth = MaxWidth String
+
+xs :: MaxWidth
+xs = MaxWidth "xs"
+
+sm :: MaxWidth
+sm = MaxWidth "sm"
+
+md :: MaxWidth
+md = MaxWidth "md"
+
+
+type DialogPropsO eff transitionProps classes =
+  ( children                :: Array ReactElement
+  , classes                 :: classes
+  , enterTransitionDuration :: Number
+  , fullscreen              :: Boolean
+  , ignoreBackdropClick     :: Boolean
+  , ignoreEscapeKeyUp       :: Boolean
+  , leaveTransitionDuration :: Number
+  , maxWidth                :: MaxWidth
+  , onBackdropClick         :: EffFn1 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Unit
+  , onEnter                 :: EffFn1 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Unit
+  , onEntered               :: EffFn1 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Unit
+  , onEntering              :: EffFn1 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Unit
+  , onEscapeKeyUp           :: EffFn1 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Unit
+  , onExit                  :: EffFn1 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Unit
+  , onExited                :: EffFn1 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Unit
+  , onExiting               :: EffFn1 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Unit
+  , onRequestClose          :: EffFn1 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Unit
+  , transition              :: ReactClass transitionProps
   )
 
 
-dialog' :: forall eff o
-         . Subrow o (DialogPropsO eff)
+type DialogClasses =
+  ( root :: Styles
+  , paper :: Styles
+  , paperWidthXs :: Styles
+  , paperWidthSm :: Styles
+  , paperWidthMd :: Styles
+  , fullscreen :: Styles
+  )
+
+
+
+dialog' :: forall eff o transitionProps classes
+         . Subrow o (DialogPropsO eff transitionProps { | classes })
+        => Subrow classes DialogClasses
         => DialogProps o -> Array ReactElement -> ReactElement
 dialog' = createElement dialogImpl

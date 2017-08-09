ef70e5375ee7df3802bd61c9f2d30e6df9dc10e0
Athan Clark <athan.clark@gmail.com>
2017-08-09 11:52:12 -0600

    react version

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -1,9 +1,39 @@
 module Main where
 
+import MaterialUI.MuiThemeProvider (muiThemeProvider')
+
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Thermite as T
+import React as R
+import React.DOM as R
+import React.DOM.Props as RP
+
+
+type State = Unit
+
+initialState = unit
+
+type Action = Unit
+
+spec :: T.Spec _ State _ Action
+spec = T.simpleSpec performAction render
+  where
+    performAction :: T.PerformAction _ State _ Action
+    performAction action props state = pure unit
+
+    render :: T.Render State _ Action
+    render dispatch props state children =
+      []
+
+
+
+main :: forall eff
+      . Eff ( console :: CONSOLE
+            | eff) Unit
 main = do
   log "Hello sailor!"
+
+  T.defaultMain spec initialState unit

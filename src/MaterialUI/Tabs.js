7906df1fcb7a9ae5bf1de055af0d2a5ce50074ba
Athan Clark <athan.clark@gmail.com>
2017-08-31 14:19:23 -0600

    tabs

---
+++ src/MaterialUI/Tabs.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var Tabs = require('material-ui/Tabs');
+
+exports.tabsImpl = Tabs.default;
+
+exports.tabImpl = Tabs.Tab;
---
+++ src/MaterialUI/Tabs.purs
@@ -0,0 +1,111 @@
+module MaterialUI.Tabs
+  ( tabs, TabsProps, TabsPropsO, TabsClasses
+  , tab, TabProps, TabPropsO, TabClasses
+  , Color (..), accent, primary, inherit, ScrollButtons, auto, on, off
+  ) where
+
+import MaterialUI.Types (Styles, Classes)
+
+import Prelude
+import React (Event, ReactClass, createElement, ReactElement, ReactProps, ReactState, ReactRefs, ReadOnly, ReadWrite)
+import Data.Record.Class (class Subrow)
+import Control.Monad.Eff.Uncurried (EffFn2)
+import Unsafe.Coerce (unsafeCoerce)
+
+
+foreign import tabsImpl :: forall props. ReactClass props
+
+
+newtype Color = Color String
+
+accent = Color "accent"
+primary = Color "primary"
+inherit = Color "inherit"
+
+newtype ScrollButtons = ScrollButtons String
+
+auto = ScrollButtons "auto"
+on = ScrollButtons "on"
+off = ScrollButtons "off"
+
+
+type TabsProps o =
+  {
+  | o }
+
+
+type TabsPropsO eff =
+  ( buttonClassName :: String
+  , centered :: Boolean
+  , children :: Array ReactElement
+  , classes :: Classes
+  , fullWidth :: Boolean
+  , indicatorClassName :: String
+  , indicatorColor :: Color
+  , onChange :: EffFn2 (props :: ReactProps, refs :: ReactRefs ReadOnly, state :: ReactState ReadWrite | eff) Event Int Unit
+  , scrollButtons :: ScrollButtons
+  , scrollable :: Boolean
+  , textColor :: Color
+  , value :: Int
+  )
+
+type TabsClasses =
+  ( root :: Styles
+  , flexContainer :: Styles
+  , scrollingContainer :: Styles
+  , fixed :: Styles
+  , scrollable :: Styles
+  , centered :: Styles
+  )
+
+
+tabs :: forall o eff
+      . Subrow o (TabsPropsO eff)
+     => TabsProps o -> Array ReactElement -> ReactElement
+tabs = createElement tabsImpl
+
+
+foreign import tabImpl :: forall props. ReactClass props
+
+type TabProps o =
+  {
+  | o }
+
+
+type TabPropsO =
+  ( classes :: Classes
+  , disabled :: Boolean
+  , icon :: ReactElement
+  , label :: ReactElement
+  , value :: Int
+  )
+
+type TabClasses =
+  ( root :: Styles
+  , flexContainer :: Styles
+  , scrollingContainer :: Styles
+  , fixed :: Styles
+  , scrollable :: Styles
+  , centered :: Styles
+  , rootLabelIcon       :: Styles
+  , rootAccent          :: Styles
+  , rootAccentSelected  :: Styles
+  , rootAccentDisabled  :: Styles
+  , rootPrimary         :: Styles
+  , rootPrimarySelected :: Styles
+  , rootPrimaryDisabled :: Styles
+  , rootInherit         :: Styles
+  , rootInheritSelected :: Styles
+  , rootInheritDisabled :: Styles
+  , fullWidth :: Styles
+  , labelContainer :: Styles
+  , label :: Styles
+  , labelWrapped :: Styles
+  )
+
+
+tab :: forall o
+      . Subrow o TabPropsO
+     => TabProps o -> ReactElement
+tab props = createElement tabImpl props []
+

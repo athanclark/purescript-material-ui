5fa10184dd3db18176072e2e0cc00fcd68fb4da3
Athan Clark <athan.clark@gmail.com>
2018-04-20 12:40:36 -0600

    chip withStyles

--- src/MaterialUI/Chip.js
+++ src/MaterialUI/Chip.js
@@ -1,5 +1,9 @@
 "use strict";
 
 var Chip = require('material-ui/Chip').default;
+var withStyles = require('material-ui/styles').withStyles;
 
 exports.chipImpl = Chip;
+exports.withStylesImpl = function withStylesImpl(stylesF,x) {
+    return withStyles(stylesF)(x);
+};
--- src/MaterialUI/Chip.purs
+++ src/MaterialUI/Chip.purs
@@ -1,15 +1,17 @@
 module MaterialUI.Chip
   ( chip, ChipProps, ChipPropsO, ChipClasses
-  , createClasses
+  , createClasses, withStyles
   ) where
 
-import MaterialUI.Types (Styles, Classes)
+import MaterialUI.Types (Styles, Classes, class CompileStyles, Theme)
 
 import Prelude
-import React (Event, ReactClass, createElement, ReactElement, ReactProps, ReactState, ReactRefs, ReadOnly, ReadWrite)
+import React (Event, ReactClass, createElement, createClassStateless, ReactElement, ReactProps, ReactState, ReactRefs, ReadOnly, ReadWrite)
 import Data.Record.Class (class Subrow)
+import Data.Function.Uncurried (Fn2, runFn2)
 import Control.Monad.Eff.Uncurried (EffFn1)
 import Unsafe.Coerce (unsafeCoerce)
+import Type.Row (class RowToList, class ListToRow)
 
 
 foreign import chipImpl :: forall props. ReactClass props
@@ -39,8 +41,18 @@ type ChipClasses =
   , deleteIcon :: Styles
   )
 
+type ChipClassesCompiled =
+  ( root :: String
+  , clickable :: String
+  , deletable :: String
+  , avatar :: String
+  , avatarChildren :: String
+  , label :: String
+  , deleteIcon :: String
+  )
+
 createClasses :: forall classes
-               . Subrow classes ChipClasses
+               . Subrow classes ChipClassesCompiled
               => { | classes } -> Classes
 createClasses = unsafeCoerce
 
@@ -49,3 +61,16 @@ chip :: forall o eff classes
          . Subrow o (ChipPropsO eff)
         => ChipProps o -> ReactElement
 chip p = createElement chipImpl p []
+
+
+
+foreign import withStylesImpl :: forall styles compiledStyles a
+                               . Fn2 (Theme -> { | styles }) (ReactClass {classes :: { | compiledStyles }}) (ReactClass a)
+
+withStyles :: forall styles stylesList compiledStyles compiledStylesList a
+            . Subrow styles ChipClasses
+            => RowToList styles stylesList
+            => CompileStyles stylesList compiledStylesList
+            => ListToRow compiledStylesList compiledStyles
+            => (Theme -> { | styles }) -> ({classes :: { | compiledStyles }} -> ReactElement) -> ReactElement
+withStyles stylesF createElem = createElement (runFn2 withStylesImpl stylesF (createClassStateless createElem)) unit []

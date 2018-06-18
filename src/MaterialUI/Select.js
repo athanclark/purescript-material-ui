6d1da26e0d0010c34ecbc95d65a3843e602e7ea4
Athan Clark <athan.clark@gmail.com>
2018-06-17 21:33:42 -0600

    with styles

--- src/MaterialUI/Select.js
+++ src/MaterialUI/Select.js
@@ -1,5 +1,9 @@
 "use strict";
 
 var Select = require('material-ui/Select').default;
+var withStyles = require('material-ui/styles').withStyles;
 
 exports.selectImpl = Select;
+exports.withStylesImpl = function withStylesImpl(stylesF,x) {
+    return withStyles(stylesF)(x);
+};
--- src/MaterialUI/Select.purs
+++ src/MaterialUI/Select.purs
@@ -3,15 +3,16 @@ module MaterialUI.Select
   , createClasses
   ) where
 
-import MaterialUI.Types (Styles, Classes)
+import MaterialUI.Types (Styles, Classes, class CompileStyles, Theme)
 import MaterialUI.Input (InputPropsO, Value)
 
 import Prelude
-import React (Event, ReactClass, createElement, ReactElement, ReactProps, ReactState, ReactRefs, ReadOnly, ReadWrite)
+import React (Event, ReactClass, createElement, createClassStateless, ReactElement, ReactProps, ReactState, ReactRefs, ReadOnly, ReadWrite)
 import Data.Record.Class (class Subrow)
-import Data.Argonaut (Json)
+import Data.Function.Uncurried (Fn2, runFn2)
 import Control.Monad.Eff.Uncurried (EffFn1)
 import Unsafe.Coerce (unsafeCoerce)
+import Type.Row (class RowToList, class ListToRow)
 
 
 foreign import selectImpl :: forall props. ReactClass props
@@ -50,8 +51,16 @@ type SelectClasses =
   , icon :: Styles
   )
 
+type SelectClassesCompiled =
+  ( root :: String
+  , select :: String
+  , selectMenu :: String
+  , disabled :: String
+  , icon :: String
+  )
+
 createClasses :: forall classes
-               . Subrow classes SelectClasses
+               . Subrow classes SelectClassesCompiled
               => { | classes } -> Classes
 createClasses = unsafeCoerce
 
@@ -61,3 +70,15 @@ select :: forall o menuProps iconComponentProps selectDisplayProps eff inputProp
         => Subrow o both
         => SelectProps o -> Array ReactElement -> ReactElement
 select = createElement selectImpl
+
+
+foreign import withStylesImpl :: forall styles compiledStyles a
+                               . Fn2 (Theme -> { | styles }) (ReactClass {classes :: { | compiledStyles }}) (ReactClass a)
+
+withStyles :: forall styles stylesList compiledStyles compiledStylesList
+            . Subrow styles SelectClasses
+            => RowToList styles stylesList
+            => CompileStyles stylesList compiledStylesList
+            => ListToRow compiledStylesList compiledStyles
+            => (Theme -> { | styles }) -> ({classes :: { | compiledStyles }} -> ReactElement) -> ReactElement
+withStyles stylesF createElem = createElement (runFn2 withStylesImpl stylesF (createClassStateless createElem)) unit []

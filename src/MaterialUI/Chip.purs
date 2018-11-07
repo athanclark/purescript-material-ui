94a9468aa0fd587e93df943cabf214255c5d072b
Athan Clark <athan.clark@gmail.com>
2018-11-06 18:07:08 -0700

    chip

--- src/MaterialUI/Chip.purs
+++ src/MaterialUI/Chip.purs
@@ -1,5 +1,7 @@
 module MaterialUI.Chip
   ( chip, ChipProps, ChipPropsO, ChipClasses
+  , Color, default, primary, secondary
+  , Variant, defaultVariant, outlined
   , createClasses, withStyles
   ) where
 
@@ -23,33 +25,93 @@ type ChipProps o =
   | o }
 
 
-type ChipPropsO eff =
+type ChipPropsO componentProps =
   ( avatar :: ReactElement
   , classes :: Classes
+  , clickable :: Boolean
+  , color :: Color
+  , component :: ReactClass componentProps -- ^ Default: React.DOM.div'
   , deleteIcon :: ReactElement
+  , icon :: ReactElement
   , label :: ReactElement
   , onDelete :: EffectFn1 SyntheticEvent Unit
   , onClick :: EffectFn1 SyntheticEvent Unit
+  , variant :: Variant
   )
+
+newtype Color = Color String
+
+default :: Color
+default = Color "default"
+
+primary :: Color
+primary = Color "primary"
+
+secondary :: Color
+secondary = Color "secondary"
+
+newtype Variant = Variant String
+
+defaultVariant :: Variant
+defaultVariant = Variant "default"
+
+outlined :: Variant
+outlined = Variant "outlined"
 
 type ChipClasses =
   ( root :: Styles
+  , colorPrimary :: Styles
+  , colorSecondary :: Styles
   , clickable :: Styles
+  , clickableColorPrimary :: Styles
+  , clickableColorSecondary :: Styles
   , deletable :: Styles
+  , deletableColorPrimary :: Styles
+  , deletableColorSecondary :: Styles
+  , outlined :: Styles
+  , outlinedColorPrimary :: Styles
+  , outlinedColorSecondary :: Styles
   , avatar :: Styles
+  , avatarColorPrimary :: Styles
+  , avatarColorSecondary :: Styles
   , avatarChildren :: Styles
+  , icon :: Styles
+  , iconColorPrimary :: Styles
+  , iconColorSecondary :: Styles
   , label :: Styles
   , deleteIcon :: Styles
+  , deleteIconColorPrimary :: Styles
+  , deleteIconColorSecondary :: Styles
+  , deleteIconOutlinedColorPrimary :: Styles
+  , deleteIconOutlinedColorSecondary :: Styles
   )
 
 type ChipClassesCompiled =
   ( root :: String
+  , colorPrimary :: String
+  , colorSecondary :: String
   , clickable :: String
+  , clickableColorPrimary :: String
+  , clickableColorSecondary :: String
   , deletable :: String
+  , deletableColorPrimary :: String
+  , deletableColorSecondary :: String
+  , outlined :: String
+  , outlinedColorPrimary :: String
+  , outlinedColorSecondary :: String
   , avatar :: String
+  , avatarColorPrimary :: String
+  , avatarColorSecondary :: String
   , avatarChildren :: String
+  , icon :: String
+  , iconColorPrimary :: String
+  , iconColorSecondary :: String
   , label :: String
   , deleteIcon :: String
+  , deleteIconColorPrimary :: String
+  , deleteIconColorSecondary :: String
+  , deleteIconOutlinedColorPrimary :: String
+  , deleteIconOutlinedColorSecondary :: String
   )
 
 createClasses :: forall classes
@@ -58,9 +120,9 @@ createClasses :: forall classes
 createClasses = unsafeCoerce
 
 
-chip :: forall o eff
-         . SubRow o (ChipPropsO eff)
-        => ChipProps o -> ReactElement
+chip :: forall o componentProps
+      . SubRow o (ChipPropsO componentProps)
+     => ChipProps o -> ReactElement
 chip p = unsafeCreateElement chipImpl p []
 
 

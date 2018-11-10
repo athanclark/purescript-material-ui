5622ba9c1c3a7e90c439bf1989e5e1024d3c6445
Athan Clark <athan.clark@gmail.com>
2018-11-10 12:17:20 -0700

    grid update

--- src/MaterialUI/Grid.purs
+++ src/MaterialUI/Grid.purs
@@ -4,7 +4,8 @@ module MaterialUI.Grid
   , AlignItems, stretchItems, centerItems, flexStartItems, flexEndItems, baselineItems
   , Direction, row, rowReverse, column, columnReverse
   , Justify, flexStartJustify, flexEndJustify, centerJustify, spaceBetweenJustify, spaceAroundJustify
-  , Spacing, spacing0, spacing8, spacing16, spacing24, spacing40
+  , Grids, falseGrids, trueGrids, autoGrids, grids1, grids2, grids3, grids4, grids5, grids6, grids7, grids8, grids9, grids10, grids11, grids12
+  , Spacing, spacing0, spacing8, spacing16, spacing24, spacing32, spacing40
   , Wrap, wrap, nowrap, wrapReverse
   , createClasses
   ) where
@@ -45,17 +46,17 @@ spaceAroundContent = AlignContent "space-around"
 
 newtype AlignItems = AlignItems String
 
-stretchItems :: AlignContent
-stretchItems = AlignContent "stretch"
+flexStartItems :: AlignContent
+flexStartItems = AlignContent "flex-start"
 
 centerItems :: AlignContent
 centerItems = AlignContent "center"
 
-flexStartItems :: AlignContent
-flexStartItems = AlignContent "flex-start"
-
 flexEndItems :: AlignContent
 flexEndItems = AlignContent "flex-end"
+
+stretchItems :: AlignContent
+stretchItems = AlignContent "stretch"
 
 baselineItems :: AlignContent
 baselineItems = AlignContent "baseline"
@@ -91,6 +92,53 @@ spaceBetweenJustify = Justify "space-between"
 spaceAroundJustify :: Justify
 spaceAroundJustify = Justify "space-around"
 
+newtype Grids = Grids Int
+
+falseGrids :: Grids
+falseGrids = Grids (unsafeCoerce false)
+
+trueGrids :: Grids
+trueGrids = Grids (unsafeCoerce true)
+
+autoGrids :: Grids
+autoGrids = Grids (unsafeCoerce "auto")
+
+grids1 :: Grids
+grids1 = Grids 1
+
+grids2 :: Grids
+grids2 = Grids 2
+
+grids3 :: Grids
+grids3 = Grids 3
+
+grids4 :: Grids
+grids4 = Grids 4
+
+grids5 :: Grids
+grids5 = Grids 5
+
+grids6 :: Grids
+grids6 = Grids 6
+
+grids7 :: Grids
+grids7 = Grids 7
+
+grids8 :: Grids
+grids8 = Grids 8
+
+grids9 :: Grids
+grids9 = Grids 9
+
+grids10 :: Grids
+grids10 = Grids 10
+
+grids11 :: Grids
+grids11 = Grids 11
+
+grids12 :: Grids
+grids12 = Grids 12
+
 newtype Spacing = Spacing Int
 
 spacing0 :: Spacing
@@ -104,6 +152,9 @@ spacing16 = Spacing 16
 
 spacing24 :: Spacing
 spacing24 = Spacing 24
+
+spacing32 :: Spacing
+spacing32 = Spacing 32
 
 spacing40 :: Spacing
 spacing40 = Spacing 40
@@ -121,59 +172,64 @@ wrapReverse = Wrap "wrap-reverse"
 
 
 type GridPropsO componentProps =
-  ( alignContent :: AlignContent
-  , raised :: Boolean
+  ( alignContent :: AlignContent -- ^ Default: `stretch`
+  , alignItems :: AlignItems -- ^ Default: `stretch`
   , classes :: Classes
-  , component :: ReactClass componentProps
-  , container :: Boolean
-  , direction :: Direction
-  , item :: Boolean
-  , justify :: Justify
-  , xl :: Int
-  , lg :: Int
-  , md :: Int
-  , sm :: Int
-  , xs :: Int
-  , spacing :: Spacing
-  , wrap :: Wrap
+  , component :: ReactClass componentProps -- ^ Default: `div`
+  , container :: Boolean -- ^ Default: `false`
+  , direction :: Direction -- ^ Default: `row`
+  , item :: Boolean -- ^ Default: `false`
+  , justify :: Justify -- ^ Default: `flex-start`
+  , xl :: Grids -- ^ Default: `false`
+  , lg :: Grids -- ^ Default: `false`
+  , md :: Grids -- ^ Default: `false`
+  , sm :: Grids -- ^ Default: `false`
+  , xs :: Grids -- ^ Default: `false`
+  , spacing :: Spacing -- ^ Default: 0
+  , wrap :: Wrap -- ^ Default: `wrap`
+  , zeroMinWidth :: Boolean -- ^ Default: `false`
   )
 
 type GridClasses =
-  ( typeContainer :: Styles
-  , typeItem :: Styles
+  ( container :: Styles
+  , item :: Styles
+  , zeroMinWidth :: Styles
   , "direction-xs-column" :: Styles
   , "direction-xs-column-reverse" :: Styles
   , "irection-xs-row-reverse" :: Styles
   , "wrap-xs-nowrap" :: Styles
-  , "lign-items-xs-center" :: Styles
+  , "wrap-xs-wrap-reverse" :: Styles
+  , "align-items-xs-center" :: Styles
   , "align-items-xs-flex-start" :: Styles
-  , "lign-items-xs-flex-end" :: Styles
+  , "align-items-xs-flex-end" :: Styles
   , "align-items-xs-baseline" :: Styles
-  , "lign-content-xs-center" :: Styles
+  , "align-content-xs-center" :: Styles
   , "align-content-xs-flex-start" :: Styles
-  , "lign-content-xs-flex-end" :: Styles
+  , "align-content-xs-flex-end" :: Styles
   , "align-content-xs-space-between" :: Styles
-  , "lign-content-xs-space-around" :: Styles
+  , "align-content-xs-space-around" :: Styles
   , "justify-xs-center" :: Styles
-  , "ustify-xs-flex-end" :: Styles
+  , "justify-xs-flex-end" :: Styles
   , "justify-xs-space-between" :: Styles
-  , "ustify-xs-space-around" :: Styles
+  , "justify-xs-space-around" :: Styles
+  , "justify-xs-space-evenly" :: Styles
   , "spacing-xs-8" :: Styles
-  , "pacing-xs-16" :: Styles
+  , "spacing-xs-16" :: Styles
   , "spacing-xs-24" :: Styles
-  , "pacing-xs-40" :: Styles
+  , "spacing-xs-32" :: Styles
+  , "spacing-xs-40" :: Styles
   , "grid-xs" :: Styles
-  , "rid-xs-1" :: Styles
+  , "grid-xs-1" :: Styles
   , "grid-xs-2" :: Styles
-  , "rid-xs-3" :: Styles
+  , "grid-xs-3" :: Styles
   , "grid-xs-4" :: Styles
-  , "rid-xs-5" :: Styles
+  , "grid-xs-5" :: Styles
   , "grid-xs-6" :: Styles
-  , "rid-xs-7" :: Styles
+  , "grid-xs-7" :: Styles
   , "grid-xs-8" :: Styles
-  , "rid-xs-9" :: Styles
+  , "grid-xs-9" :: Styles
   , "grid-xs-10" :: Styles
-  , "rid-xs-11" :: Styles
+  , "grid-xs-11" :: Styles
   , "grid-xs-12" :: Styles
   )
 
